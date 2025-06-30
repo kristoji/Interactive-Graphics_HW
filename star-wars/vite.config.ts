4b94cc37acbfbe516f77360341725ccdb8a50f36
kristoji <kristar301@gmail.com>
2025-06-30 18:57:16 +0200

    almost

--- star-wars/vite.config.ts
+++ star-wars/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Interactive-Graphics_HW/star-wars/',
+  base: '/Interactive-Graphics_HW/star-wars/dist/',
   // …any other config…
 })
